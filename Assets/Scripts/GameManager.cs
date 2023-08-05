3be7d6f74b2403d12624d10444ed1b6fdd9c312e
Filip Duler <filip.duler@gmail.com>
2023-08-05 20:05:32 +0200

    feat: better client management

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,10 @@
 using Assets.Scripts.Client;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     void Start()
     {
-        Client.Instance.SetEnabled(true);
-    }
-
-    private void Reset()
-    {
-        Client.Instance.SetEnabled(false);
+        Client.Instance.SetProcessPacketState(true);
     }
 }
