72cad17673a2ca8e943e91575c1119369a8e0b91
Sally510 <wangqingyu510@gmail.com>
2023-08-05 20:07:11 +0800

    inventory

--- Assets/Scripts/Components/InventoryItem.cs
+++ Assets/Scripts/Components/InventoryItem.cs
@@ -1,9 +1,47 @@
 using UnityEngine;
+using UnityEngine.UI;
 using Assets.Scripts;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using TMPro;
 
-public class InventoryItem
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler ,IEndDragHandler  
 {
-    public Item item;
-    public int quantity;
-    public int slot;
+    public Image image;
+    public TMP_Text itemName;
+    public TMP_Text itemQuantity;
+    [HideInInspector] public Transform parentAfterDrag;
+
+    public void InitializeItem(string name, int quantity)
+    {
+        itemName.text = name;
+        itemQuantity.text = quantity.ToString();
+
+    }
+
+    // Drag and drop
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = false;
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = true;
+        transform.SetParent(parentAfterDrag);
+    }
+
+
+
+
+
+
 }
---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    //[SerializeField]
+    //private InventoryPage _inventoryPage;
+    public InventorySlot[] inventorySlots;
+    
+    public void AddItem(InventoryItem item)
+    {
+        // Find any empty slot
+        for (int i = 0; i < inventorySlots.Length; i++)
+        {
+            InventorySlot slot = inventorySlots[i];
+            InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
+            if (itemInSlot == null)
+            {
+                SpawnNewItem(item, slot);
+                return;
+            }
+        }
+    }
+
+    void SpawnNewItem(InventoryItem item, InventorySlot slot)
+    {
+        //GameObject newItem = Instantiate(inventoryItemPrefab, slot.transform);
+
+    }
+
+    public void Start()
+    {
+    }
+}
