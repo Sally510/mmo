7ae3efcc61804e3b6f535dd73db80b6a3f8c3aa5
Filip Duler <filip.duler@gmail.com>
2023-08-07 20:00:27 +0200

    feat: health bar

--- Assets/Scripts/Enemy/EnemyManager.cs
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -12,77 +12,15 @@ namespace Assets.Scripts.Enemy
         private GameObject enemyPrefab;
         private readonly Dictionary<uint, EnemyObject> enemies = new();
 
-        void FixedUpdate()
-        {
-            foreach (EnemyObject obj in enemies.Values)
-            {
-                if (obj.AutoMovement != null)
-                {
-                    if (obj.AutoMovement.IsDone)
-                    {
-                        obj.AutoMovement = null;
-                        obj.StopMove();
-                    }
-                    else
-                    {
-                        obj.AutoMovement.Update(Time.fixedDeltaTime);
-                        Vector3 worldPos = obj.AutoMovement.CurrentIsoPosition().FromIsoToWorld();
-                        obj.MoveTo(worldPos);
-                    }
-                }
-            }
-        }
 
         void OnEnable()
         {
             PacketEventHandler.MonsterInfoEvent += PacketEventHandler_MonsterInfoEvent;
-            PacketEventHandler.MonsterChangeEvent += PacketEventHandler_MonsterChangeEvent;
-            PacketEventHandler.AutoWalkEvent += PacketEventHandler_AutoWalkEvent;
         }
 
         private void OnDisable()
         {
             PacketEventHandler.MonsterInfoEvent -= PacketEventHandler_MonsterInfoEvent;
-            PacketEventHandler.MonsterChangeEvent -= PacketEventHandler_MonsterChangeEvent;
-            PacketEventHandler.AutoWalkEvent -= PacketEventHandler_AutoWalkEvent;
-        }
-
-        private void PacketEventHandler_AutoWalkEvent(object sender, AutoWalkModel e)
-        {
-            if (enemies.TryGetValue(e.EntityId, out EnemyObject enemy))
-            {
-                enemy.AutoMovement = new AutoMovementManager(e.StartTime, e.WalkDuration, e.Moves);
-            }
-        }
-
-        private void PacketEventHandler_MonsterChangeEvent(object sender, MonsterChangeListModel e)
-        {
-            foreach (MonsterChangeListModel.MonsterChange change in e.Changes)
-            {
-                if (change.HasFlag(ChangeState.Spawn))
-                {
-                    Debug.Log($"Spawn: {change.EntityId}");
-                    if (enemies.TryGetValue(change.EntityId, out EnemyObject enemy))
-                    {
-                        //monster.GetComponent<Rigidbody2D>().MovePosition(change.Position.FromIsoToWorld());
-
-                        //monster.Direction = change.Direction;
-                        //monster.Heal(change.Health);
-                    }
-                }
-                if (change.HasFlag(ChangeState.Damaged))
-                {
-                    Debug.Log($"Damaged: {change.EntityId}");
-                }
-                if (change.HasFlag(ChangeState.Died))
-                {
-                    Debug.Log($"Died: {change.EntityId}");
-                }
-                if (change.HasFlag(ChangeState.Attack))
-                {
-                    Debug.Log($"Entity {change.EntityId} attacked!");
-                }
-            }
         }
 
         private void PacketEventHandler_MonsterInfoEvent(object sender, MonsterListModel e)
--- Assets/Scripts/Enemy/EnemyObject.cs
+++ Assets/Scripts/Enemy/EnemyObject.cs
@@ -1,16 +1,19 @@
-﻿using Assets.Scripts.Client.Models;
+﻿using Assets.Scripts.Client;
+using Assets.Scripts.Client.Models;
+using Assets.Scripts.Helpers;
 using Assets.Scripts.Player;
-using System.Threading;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Enemy
 {
-    public class EnemyObject: MonoBehaviour
+    public class EnemyObject : MonoBehaviour
     {
         public MonsterModel Monster { get; private set; }
         private Rigidbody2D _rigidBody;
         private PlayerAnimation _animation;
-        public AutoMovementManager AutoMovement { get; set; }
+        private HealthBar _healthBar;
+        private AutoMovementManager _autoMovement;
 
         private void Start()
         {
@@ -21,6 +24,28 @@ namespace Assets.Scripts.Enemy
         public void Initialize(MonsterModel monster)
         {
             Monster = monster;
+
+            _healthBar = GetComponentInChildren<HealthBar>();
+            _healthBar.SetMaxHealth(monster.MaxHealth);
+            _healthBar.SetHealth(monster.Health);
+        }
+
+        void FixedUpdate()
+        {
+            if (_autoMovement != null)
+            {
+                if (_autoMovement.IsDone)
+                {
+                    _autoMovement = null;
+                    StopMove();
+                }
+                else
+                {
+                    _autoMovement.Update(Time.fixedDeltaTime);
+                    Vector3 worldPos = _autoMovement.CurrentIsoPosition().FromIsoToWorld();
+                    MoveTo(worldPos);
+                }
+            }
         }
 
         public void MoveTo(Vector2 worldPosition)
@@ -33,6 +58,53 @@ namespace Assets.Scripts.Enemy
         public void StopMove()
         {
             _animation.SetDirection(Vector2.zero);
+        }
+
+        void OnEnable()
+        {
+            PacketEventHandler.MonsterChangeEvent += PacketEventHandler_MonsterChangeEvent;
+            PacketEventHandler.AutoWalkEvent += PacketEventHandler_AutoWalkEvent;
+        }
+
+        private void OnDisable()
+        {
+            PacketEventHandler.MonsterChangeEvent -= PacketEventHandler_MonsterChangeEvent;
+            PacketEventHandler.AutoWalkEvent -= PacketEventHandler_AutoWalkEvent;
+        }
+
+        private void PacketEventHandler_AutoWalkEvent(object sender, AutoWalkModel e)
+        {
+            if (Monster.EntityId == e.EntityId)
+            {
+                _autoMovement = new AutoMovementManager(e.StartTime, e.WalkDuration, e.Moves);
+            }
+        }
+
+        private void PacketEventHandler_MonsterChangeEvent(object sender, MonsterChangeListModel e)
+        {
+            foreach (MonsterChangeListModel.MonsterChange change in e.Changes.Where(x => x.EntityId == Monster.EntityId))
+            {
+                if (change.HasFlag(ChangeState.Spawn))
+                {
+                    Debug.Log($"Spawn: {change.EntityId}");
+                    _healthBar.SetHealth(change.Health);
+                    _rigidBody.MovePosition(change.Position.FromIsoToWorld());
+                }
+                if (change.HasFlag(ChangeState.Damaged))
+                {
+                    Debug.Log($"Damaged: {change.EntityId}");
+                    _healthBar.SetHealth(change.Health);
+                }
+                if (change.HasFlag(ChangeState.Died))
+                {
+                    Debug.Log($"Died: {change.EntityId}");
+                    
+                }
+                if (change.HasFlag(ChangeState.Attack))
+                {
+                    Debug.Log($"Entity {change.EntityId} attacked!");
+                }
+            }
         }
     }
 }
