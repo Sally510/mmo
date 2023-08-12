1d0f875974ab1924b83924771d7de0ddf1a34a8c
Sally510 <wangqingyu510@gmail.com>
2023-08-13 03:00:48 +0800

    Implemented character badge

--- Assets/Scripts/CharacterBadgeManager.cs
+++ Assets/Scripts/CharacterBadgeManager.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts;
+using Assets.Scripts.Helpers;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -8,13 +9,25 @@ public class CharacterBadgeManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text characterName; 
     [SerializeField] private TMP_Text levelText;
+    [SerializeField] private TMP_Text healthValue;
+    [SerializeField] private TMP_Text expValue;
     [SerializeField] private ProgressBar characterHealthBar;
     [SerializeField] private ProgressBar expBar;
+
     private void Start()
     {
         characterName.text = State.LoggedCharacter.option.Name;
-        //expBar.SetHealth((int)State.LoggedCharacter.option.Experience);
     }
 
-
+    private void Update()
+    {
+        levelText.text = "LV" + State.LoggedCharacter.character.Level.ToString();
+        characterHealthBar.SetMaxValue(State.LoggedCharacter.character.MaxHealth);
+        characterHealthBar.SetValue(State.LoggedCharacter.character.Health);
+        var (start, end) = ExperienceHelpers.CalculateExperienceRangeAtLevel(State.LoggedCharacter.character.Level);
+        expBar.SetMaxValue((int)end - (int)start);
+        expBar.SetValue((int)(State.LoggedCharacter.character.Experience - start));
+        healthValue.text = State.LoggedCharacter.character.Health.ToString() + " / " + State.LoggedCharacter.character.MaxHealth.ToString();
+        expValue.text = (State.LoggedCharacter.character.Experience - start).ToString() + " / " + ((int)end - (int)start).ToString();
+    }
 }
